8d577bbbdaa64dffb537135b7beef5c526f38f27
mukul1em <mukulrt1997@gmail.com>
2025-01-01 15:06:33 -0500

    Initial Commit

---
+++ JPApexPredator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JPApexPredator
+//
+//  Created by Mukul Rawat on 1/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JPApexPredator/JPApexPredatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorApp.swift
+//  JPApexPredator
+//
+//  Created by Mukul Rawat on 1/1/25.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
