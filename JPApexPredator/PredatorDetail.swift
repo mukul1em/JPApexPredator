eb887318e72034fae91965c7917f47f50252093b
mukul1em <mukulrt1997@gmail.com>
2025-01-01 21:40:19 -0500

    update

---
+++ JPApexPredator/PredatorDetail.swift
@@ -0,0 +1,18 @@
+//
+//  PredatorDetail.swift
+//  JPApexPredator
+//
+//  Created by Mukul Rawat on 1/1/25.
+//
+
+import SwiftUI
+
+struct PredatorDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PredatorDetail()
+}
---
+++ JPApexPredator/PredatorMap.swift
@@ -0,0 +1,18 @@
+//
+//  PredatorMap.swift
+//  JPApexPredator
+//
+//  Created by Mukul Rawat on 1/1/25.
+//
+
+import SwiftUI
+
+struct PredatorMap: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PredatorMap()
+}
